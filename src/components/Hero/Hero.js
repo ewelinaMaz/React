bfddcf955dac172a6520104f56cbc967b5d78c0b
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-03-04 12:24:45 +0100

    Fix image add

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import styles from './Hero.scss';
 const Hero = () => (
   <header className={styles.component}>
     <h2 className={styles.title}>Things to do</h2>
-    <img className={styles.image} src="../images/space.png" alt="space"/>
+    <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="space"/>
   </header>
 );
 
