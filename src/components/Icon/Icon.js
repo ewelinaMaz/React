5d881c10639cd5da9a1de084fa703ecfa4d181e2
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-03-16 11:14:27 +0100

    Test

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,10 +1,7 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
 
-Icon.propTypes = {
-  name: PropTypes.node,
-};
 
 export default Icon;
